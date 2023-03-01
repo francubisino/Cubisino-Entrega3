08df1f454e5e8566305c7f64bd6b2fbe08f35c81
Francisco <francubisino93@gmail.com>
2023-03-01 09:43:24 -0300

    Cubisino-ProyectoFinal v.1.0

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+#from django.urls import URLPattern
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = '/img/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'AppCoder/static/AppCoder/assets')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,8 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR,"static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from AppCoder import views
+from django.conf import settings
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppCoder/', include('AppCoder.urls')),
-    ]
+    #path('alojamientos/buscar_alojamiento/<int:id>',views.alojamientos)
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#Para poder visualizar las imagenes cargadas
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
